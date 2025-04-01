332e60eeb754f284589501e2db6e4e157e90cc6e
João Xavier <fastjonh@gmail.com>
2025-04-01 03:33:58 -0300

    saving frames on folder and building rust shared lib

--- doomgeneric/binds/src/lib.rs
+++ doomgeneric/binds/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{
     ffi::CStr,
+    fs::create_dir,
     slice,
     sync::OnceLock,
     thread,
@@ -19,6 +20,7 @@ static START: OnceLock<Instant> = OnceLock::new();
 #[unsafe(no_mangle)]
 pub extern "C" fn DG_Init() {
     START.set(Instant::now()).ok();
+    create_dir("frames/").ok();
     println!("DG_Init: called");
 }
 
@@ -44,7 +46,7 @@ pub extern "C" fn DG_DrawFrame() {
 
     let img = RgbImage::from_vec(640, 400, img).unwrap();
 
-    let file = format!("frame-{}.png", START.get().unwrap().elapsed().as_millis());
+    let file = format!("frames/{}.png", get_elapsed_ms());
 
     img.save(file).unwrap();
 }
@@ -70,7 +72,8 @@ pub extern "C" fn DG_GetTicksMs() -> u32 {
 /// confia papai
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn DG_GetKey(pressed: *mut i32, doom_key: *const u8) -> i32 {
-    println!("DG_GetKey: Checking {}", unsafe { *doom_key } as char);
+    let key = unsafe { *doom_key } as char;
+    println!("DG_GetKey: Checking {key}");
     unsafe { *pressed = 0 }
     0
 }
@@ -82,4 +85,8 @@ pub unsafe extern "C" fn DG_SetWindowTitle(title: *const i8) {
     let title = unsafe { CStr::from_ptr(title) }.to_str().unwrap();
 
     println!("DG_SetWindowTitle: Changing title to {title}");
+}
+
+fn get_elapsed_ms() -> u128 {
+    START.get().unwrap().elapsed().as_millis()
 }
--- doomgeneric/build.sh
+++ doomgeneric/build.sh
@@ -1,1 +1,2 @@
+cargo build --manifest-path binds/Cargo.toml
 gcc -o doom src/*.c -Lbinds/target/debug -lbinds -Wl,-rpath,binds/target/debug
