e72245974b4405e2cc2236cd6572c458a82aa61f
João Xavier <fastjonh@gmail.com>
2025-04-16 11:39:00 -0300

    adding submodule (just to avoid 50k sloc of C on my github stats)

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 cargo build --manifest-path rs_bind/Cargo.toml
-gcc -o doom src/*.c -Lrs_bind/target/debug -lbinds -Wl,-rpath,rs_bind/target/debug
+
+gcc -o doom ./doomgeneric/src/*.c -Lrs_bind/target/debug -lrs_bind -Wl,-rpath,rs_bind/target/debug
